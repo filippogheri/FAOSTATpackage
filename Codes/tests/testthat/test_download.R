c4dd5f4a7c38eced35fbb94580b0aba6921dd809
filippogheri <filippo.gheri@hotmail.com>
2017-01-30 17:20:02 +0100

    Adding new testthat file

---
+++ Codes/tests/testthat/test_download.R
@@ -0,0 +1,50 @@
+library(FAOSTAT)
+library(data.table)
+
+## Production domain
+production.dt <- 
+  data.table(varName = c("Grapes_AreaHarv", 
+                         "Wine_ProdQuantity", 
+                         "CattleBuffaloes_Stocks", 
+                         "MilkProducts_Yield",
+                         "Yoghurt_ProdQuantity",
+                         "Food _GPIN",
+                         "Agriculture_NetProdValue"),
+             domainCode = c("QC", "QD", "QA", "QL", "QP", "QI", "QV"),
+             itemCode = c(560, 564, 1746, "1780>", 891, 2054, 2051),
+             elementCode = c(2312, 2510, 2111, 2413, 2510, 432, 154))
+production.lst <- with(production.dt,
+                       FAOSTAT(name = varName, domainCode = domainCode,
+                               itemCode = itemCode, elementCode = elementCode,
+                               yearRange = c(2005:2010), countrySet = c(106, 5400)))
+
+## Trade
+tradeFlow.dt <- 
+  data.table(varName = c("Wine_ImpVal", 
+                         "Sawnwood_Imp"),
+             domainCode = c("TM", "FT"),
+             itemCode = c(564, 1632),
+             elementCode = c(2620, 2610))
+tradeFlow.lst <- with(tradeFlow.dt,
+                      FAOSTATtrade(name = varName, domainCode = domainCode,
+                                   itemCode = itemCode, elementCode = elementCode,
+                                   yearRange = c(2010), countrySet = c(68, 106)))
+
+# fillCountryCode function ------------------------------------------------
+
+test.dt <- 
+  FAOSTAT(name = c("Grapes_AreaHarv"), domainCode = "QC",
+          itemCode = 560, elementCode = 2312, yearRange = 2010)
+test.dt <- test.dt$entity
+test.dt <- 
+  fillCountryCode(data = test.dt, country = "Country", outCode = "ISO2_CODE")
+
+# translateCountryCode function -------------------------------------------
+
+test.dt <- 
+  FAOSTAT(name = c("Grapes_AreaHarv"), domainCode = "QC",
+          itemCode = 560, elementCode = 2312, yearRange = 2010)
+test.dt <- test.dt$entity
+test.dt <- 
+  translateCountryCode(data = test.dt, from = "FAOST_CODE", 
+                       to = "ISO2_CODE")
